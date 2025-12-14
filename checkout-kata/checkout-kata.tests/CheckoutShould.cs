abdd5aba45d94f5c67e57d9e7a388a86bf705d46
David Hobson <david.hobson@spektrix.com>
2025-12-14 14:20:38 +0000

    Ensure a simple checkout returns single item D only

---
+++ checkout-kata/checkout-kata.tests/CheckoutShould.cs
@@ -0,0 +1,17 @@
+﻿namespace checkout_kata.tests;
+
+public class CheckoutShould
+{
+    [Test]
+    public void Give_The_Total_Price_Of_A_Single_Scanned_Item()
+    {
+        //Arrange
+        ICheckout checkout = new Checkout();
+
+        //Act
+        checkout.Scan("D");
+        
+        //Assert
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(15));
+    }
+}
---
+++ checkout-kata/checkout-kata/Checkout.cs
@@ -0,0 +1,19 @@
+﻿namespace checkout_kata;
+
+public interface ICheckout
+{
+    void Scan(string item);
+    int GetTotalPrice();
+}
+
+public class Checkout : ICheckout
+{
+    public void Scan(string item)
+    {
+    }
+
+    public int GetTotalPrice()
+    {
+        return 15;
+    }
+}
