1cfec572bab8d6cbcdf2f0a3ffc8b1f1dd22337d
David Hobson <david.hobson@spektrix.com>
2025-12-14 14:42:14 +0000

    Refactor second test with testcase implementation

--- checkout-kata/checkout-kata.tests/CheckoutShould.cs
+++ checkout-kata/checkout-kata.tests/CheckoutShould.cs
@@ -2,6 +2,18 @@
 
 public class CheckoutShould
 {
+    public class TestCase
+    {
+        public TestCase(string[] input, int expectedOutput)
+        {
+            Input = input;
+            ExpectedOutput = expectedOutput;
+        }
+
+        public string[] Input { get; set; }
+        public int ExpectedOutput { get; set; }
+    }  
+    
     [TestCase("A", 50)]
     [TestCase("B", 30)]
     [TestCase("C", 20)]
@@ -18,17 +30,27 @@ public class CheckoutShould
         Assert.That(checkout.GetTotalPrice(), Is.EqualTo(expected));
     }
     
-    [Test]
-    public void Give_The_Total_Price_Of_Multiple_Scanned_Items()
+    public static List<TestCase> MultipleItemCases()
+    {
+        var cases = new List<TestCase>();
+        cases.Add(new TestCase(["C", "C"], 40));
+        cases.Add(new TestCase(["D", "D"], 30));
+        return cases;
+    }      
+    
+    [TestCaseSource(nameof(MultipleItemCases))]
+    public void Give_The_Total_Price_Of_Multiple_Scanned_Items(TestCase testCase)
     {
         //Arrange
         ICheckout checkout = new Checkout();
 
         //Act
-        checkout.Scan("D");
-        checkout.Scan("D");
+        foreach (var item in testCase.Input)
+        {
+            checkout.Scan(item);
+        }
         
         //Assert
-        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(30));
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(testCase.ExpectedOutput));
     }
 }
